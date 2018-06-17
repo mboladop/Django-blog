0b25c80e7d812072f840ca50b61200ccddbaa465
Richard Dalton <richard@codeinstitute.net>
2018-06-17 00:29:03 +0000

    Initial Commit

---
+++ posts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import BlogPostForm
+
+def get_posts(request):
+    posts = Post.objects.all()
+    return render(request, "posts/blogposts.html", {'posts': posts})
+
+def post_detail(request, pk):
+    """
+    Create a view that returns a single
+    Post object based on the post ID (pk) and
+    render it to the 'postdetail.html' template.
+    Or return a 404 error if the post is
+    not found
+    """
+    post = get_object_or_404(Post, pk=pk)
+    post.views += 1
+    post.save()
+    return render(request, "posts/postdetail.html", {'post': post})
+
+def create_or_edit_post(request, pk=None):
+    """
+    Create a view that allows us to create
+    or edit a post depending if the Post ID
+    is null or not
+    """
+    post = get_object_or_404(Post, pk=pk) if pk else None
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', post.pk)
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'posts/blogpostform.html', {'form': form})
