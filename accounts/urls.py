f5076a6d7a11f5dd2f372ac7493fb3838f953bb8
Luisa Bolado <mboladop@hotmail.com>
2018-06-19 10:36:09 +0000

    bug in urls.py in accounts fixed from when changing from urls to path

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from accounts.views import login, register, logout, profile
 
 #ACCOUNTS
 urlpatterns = [
-    path('accounts/login', login, name = 'login'),
-    path('^accounts/register', register, name = 'register'),
-    path('^accounts/logout', logout, name = 'logout'),
-    path('^accounts/profile', profile, name = 'profile')
+    path('login', login, name = 'login'),
+    path('register', register, name = 'register'),
+    path('logout', logout, name = 'logout'),
+    path('profile', profile, name = 'profile')
     ]
